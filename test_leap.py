4627fadba726993bfab82a1d0ff8736680d3fb3b
Fe1c <tguyofawesome@gmail.com>
2021-05-22 23:08:35 -0700

    Final Commit with PDF

--- test_leap.py
+++ test_leap.py
@@ -10,11 +10,16 @@ class TestCase(unittest.TestCase):
         self.assertEqual(leap.leap_year(2012), 1)
     def test_pass_2(self):
         self.assertEqual(leap.leap_year(2008), 1)
+    def test_pass_3(self):
+        self.assertEqual(leap.leap_year(400), 1)
+        
     #fail cases
     def test_fail_1(self):
         self.assertEqual(leap.leap_year(1999), 1)
     def test_fail_2(self):
         self.assertEqual(leap.leap_year(2016), 0)
+    def test_fail_3(self):
+        self.assertEqual(leap.leap_year(401), 1)
 
 
 if __name__ == '__main__':
